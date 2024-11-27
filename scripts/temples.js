bd52504bcc0e4c76801b8459a7a597f1c8769d49
Kade O'Crowley <kadeocrowley@gmail.com>
2024-11-26 19:16:29 -0700

    layout complete

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,4 +2,12 @@ const currentYear = new Date().getFullYear();
 document.getElementById("copyright").innerText += currentYear  + "- Kade O'Crowley - USA";
 
 const lastModifiedDate = new Date(document.lastModified);
-document.getElementById("lastModified").innerText += lastModifiedDate.toLocaleString();
+document.getElementById("lastModified").innerText += lastModifiedDate.toLocaleString(); 
+
+const menuBtn = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
+
+menuBtn.addEventListener('click', function() {
+    navigation.classList.toggle('show')
+    menuBtn.classList.toggle('show')
+})
